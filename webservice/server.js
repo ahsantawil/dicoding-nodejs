b118182f75dd4f9519e1f328a39859747f53b5f7
ahsantawil <ahsan.tawil07@gmail.com>
2022-07-12 15:35:13 +0700

    Routing request

--- webservice/server.js
+++ webservice/server.js
@@ -4,19 +4,34 @@ const requestListener = (req, res) => {
     res.setHeader(`Content-type`, `text/html`);
     res.statusCode = 200;
 
-    const { method } = req;
+    const { method, url } = req;
 
-    if (method === 'POST') {
-        let body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    if (url === '/') {
+        if (method === 'GET') {
+            res.end(`<h1>Ini adalah halaman Homepage</h1>`)
+        } else {
+            res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+        }
+    } else if (url === '/about') {
+         if (method === 'GET') {
+            res.end(`<h1> Hallo ini adalah halaman ABOUT </h1>`);
+         } else if(method === 'POST') {
+                 let body = [];
 
-        req.on('end', ()=> {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            res.end(`<h2>Hi, ${name} !!</h2>`)
-        })
+                 req.on('data', (chunk) => {
+                     body.push(chunk);
+                 });
+         
+                 req.on('end', ()=> {
+                     body = Buffer.concat(body).toString();
+                     const { name } = JSON.parse(body);
+                     res.end(`<h2>Hi, ${name} !!</h2>`)
+                 })
+         } else {
+            res.end(`<h1>Halaman yang anda tuju tidak menggunakan ${method} request </h1>`);
+         }
+    } else {
+        res.end(`<h1>Halaman tidak ditemukan !!</1>`)
     }
     
 };
